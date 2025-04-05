596cead93313a4c90e910f6c5534560b67b454c5
Utkarsh Patel <pateluttkarsh697@gmail.com>
2025-04-05 19:17:22 +0530

    added schema

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const shapesTypes = [
+  "rectangle",
+  "circle",
+  "line",
+  "freehand",
+  "arrow",
+  "text",
+];
+
+const shapeSchema = new mongoose.Schema({
+  _id: String,
+  shapes: [
+    {
+      type: { type: String, enum: shapesTypes },
+      start: { x: Number, y: Number, _id: false },
+      end: { x: Number, y: Number, _id: false },
+      points: [{ x: Number, y: Number, _id: false }],
+      width: Number,
+      height: Number,
+      centerX: Number,
+      centerY: Number,
+      radius: Number,
+      content: String,
+      lineWidth: Number,
+      color: String,
+      fontSize: Number,
+    },
+  ],
+});
+
+export const Shapes = mongoose.model("user", shapeSchema);
